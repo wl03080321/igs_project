e1ef4cc15d0bcd6946b50f73c3a082194e419a36
wl03080321 <wl0308032129@gmail.com>
2025-07-14 13:03:22 +0800

    [修改] 1.修改attachments不存在問題 2.修改Readme.md

--- Auto_mail/src/script.py
+++ Auto_mail/src/script.py
@@ -7,6 +7,10 @@ def load_config(config_path):
     if not os.path.exists(abs_config_path):
         print("using default config")
         abs_config_path = Path(__file__).parent.parent / 'config' / 'config.yaml'
-    with open(abs_config_path, 'r') as f:
-        config = yaml.safe_load(f)
+    try:
+        with open(abs_config_path, 'r') as f:
+            config = yaml.safe_load(f)
+    except yaml.YAMLError as e:
+        print(f"Error loading YAML configuration: {e}")
+        raise
     return config
